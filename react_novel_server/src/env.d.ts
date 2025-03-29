9e3fece0a97735e1b8e438c24c7eed6c4ddc199a
coderjc <2740954762@qq.com>
2025-03-30 02:19:24 +0800

    feat: add env.d.ts for server config

---
+++ react_novel_server/src/env.d.ts
@@ -0,0 +1,10 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+        PORT: string
+        DB_NAME: string
+        DB_PORT: string
+        DB_DATABASE: string
+        DB_USERNAME: string
+        DB_PASSWORD: string
+    }
+}
