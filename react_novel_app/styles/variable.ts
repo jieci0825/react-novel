4d867174b56d1c9d3826abd48bbb8da77ab6cf59
coderjc <2740954762@qq.com>
2025-04-09 12:15:02 +0800

    feat: 修改主题颜色

--- react_novel_app/styles/variable.ts
+++ react_novel_app/styles/variable.ts
@@ -1,25 +1,25 @@
 export const LightTheme = {
-  bgColor: "#ffffff",
-  bgSecondaryColor: "#F5F5F5",
-  primaryColor: "#181718",
-  secondaryColor: "#4D4D4D",
-  tertiaryColor: "#666666",
-  activeBgColor: "#EFEFEF",
-  textPrimaryColor: "#171717",
-  textSecondaryColor: "#525252",
-  textTertiaryColor: "#ced6e0",
-  borderColor: "#eee",
-};
+    bgColor: '#ffffff',
+    bgSecondaryColor: '#F5F5F5',
+    primaryColor: '#333333',
+    secondaryColor: '#4D4D4D',
+    tertiaryColor: '#666666',
+    activeBgColor: '#EFEFEF',
+    textPrimaryColor: '#171717',
+    textSecondaryColor: '#525252',
+    textTertiaryColor: '#ced6e0',
+    borderColor: '#eee'
+}
 
 export const DarkTheme = {
-  bgColor: "#121212",
-  primaryColor: "#ffffff",
-  secondaryColor: "#bbbbbb",
-  tertiaryColor: "#888888",
-  activeBgColor: "#333333",
-  textPrimaryColor: "#ffffff",
-  textSecondaryColor: "#bbbbbb",
-  textTertiaryColor: "#666666",
-  borderColor: "#444444",
-  bgSecondaryColor: "#333333",
-};
+    bgColor: '#121212',
+    primaryColor: '#ffffff',
+    secondaryColor: '#bbbbbb',
+    tertiaryColor: '#888888',
+    activeBgColor: '#333333',
+    textPrimaryColor: '#ffffff',
+    textSecondaryColor: '#bbbbbb',
+    textTertiaryColor: '#666666',
+    borderColor: '#444444',
+    bgSecondaryColor: '#333333'
+}
