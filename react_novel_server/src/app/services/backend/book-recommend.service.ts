9db45495253709e466336306cc8d5ba9951484f1
coderjc <2740954762@qq.com>
2025-04-09 23:18:07 +0800

    feat: 修改推荐书籍类型为枚举

--- react_novel_server/src/app/services/backend/book-recommend.service.ts
+++ react_novel_server/src/app/services/backend/book-recommend.service.ts
@@ -1,10 +1,11 @@
 import BookRecommendModel from '@/app/models/book-recommend.model'
 import { AddBookRecommendParams } from '@/app/types/backend/book-recommend.type'
+import { BookRecommendType } from '@/app/types/common.type'
 
 // 添加站主推荐书籍
 export async function addMainBookRecommendService(data: AddBookRecommendParams) {
     const insertData = {
-        recommend_type: 'main',
+        recommend_type: BookRecommendType.MAIN,
         book_name: data.bookName,
         book_id: data.bookId,
         book_cover: data.bookCover,
