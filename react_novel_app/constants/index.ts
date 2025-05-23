bf6217f5a603bd888f73f7a5997b08199fdc4d75
coderjc <2740954762@qq.com>
2025-05-23 15:28:53 +0800

    refactor(constants): 清理未使用的常量定义并添加阅读器配置 移除未使用的常量如书架和阅读进度相关键，新增小说阅读器界面配置常量

--- react_novel_app/constants/index.ts
+++ react_novel_app/constants/index.ts
@@ -4,26 +4,20 @@ export const REFRESH_TOKEN = 'REFRESH_TOKEN'
 // 当前书源
 export const CURRENT_SOURCE = 'CURRENT_SOURCE'
 
+// 小说阅读器界面配置
+export const NOVEL_READER_CONFIG = 'NOVEL_READER_CONFIG'
+
 // 搜索历史key
 export const SEARCH_HISTORY = 'SEARCH_HISTORY'
-
-// 小说阅读进度key
-export const NOVEL_READ_PROGRESS = 'NOVEL_READ_PROGRESS'
 
 // 小说阅读历史key
 export const NOVEL_READ_HISTORY = 'NOVEL_READ_HISTORY'
 
-// 我的书架key
-export const MY_BOOKSHELF = 'MY_BOOKSHELF'
-
 // 书架布局key
 export const BOOKSHELF_LAYOUT = 'BOOKSHELF_LAYOUT'
-
-// 当前阅读章节(即阅读进度)
-export const CURRENT_READ_CHAPTER_KEY = 'CURRENT_READ_CHAPTER'
 
 // 阅读器界面设置
 export const READER_SETTING = 'READER_SETTING'
 
-// 热度器区域划分指引
+// 阅读器区域划分指引
 export const READER_GUIDE_AREA = 'READER_GUIDE_AREA'
